191208f186e86059962e0b25a13eb615ec0a60da
Jason Bert <jason.bert@goblinrockets.com>
2019-09-13 10:17:10 +0100

    Started to implement other tables to house contract data

--- src/migrationSequences/createTables.sql
+++ src/migrationSequences/createTables.sql
@@ -3,3 +3,22 @@ CREATE TABLE IF NOT EXISTS "${schema^}"."match_type" (
     "type" VARCHAR NOT NULL,
     "max_opponents" INTEGER NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS "${schema^}"."match" (
+    "id" CHAR(64) NOT NULL,
+    "match_type_id" CHAR(64),
+    "title" VARCHAR NOT NULL,
+    "owner" VARCHAR NOT NULL,
+    "created" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    PRIMARY KEY (id, match_type_id),
+    FOREIGN KEY (match_type_id) REFERENCES match_type(id)
+);
+
+CREATE TABLE IF NOT EXISTS "${schema^}"."match_opponent" (
+    "id" CHAR(64) NOT NULL,
+    "match_id" CHAR(64),
+    "type" VARCHAR NOT NULL,
+    "max_opponents" INTEGER NOT NULL,
+    PRIMARY KEY (id, match_type_id),
+    FOREIGN KEY (match_type_id) REFERENCES match_type(id)
+);
